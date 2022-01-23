142e31884ec124f17825d8efea6b152cc2d8a09d
tom2951 <92095714+tom2951@users.noreply.github.com>
2022-01-23 22:26:09 +0900

    表示項目修正

--- Process/_result.php
+++ Process/_result.php
@@ -1,21 +1,115 @@
-<?php 
-  /*
-  入力値
-  生年月日から現在の年齢取得
-  片道通勤時間
-  年間休日
-  定年予定年齢
+<?php
 
+// // (1) 登録するデータを用意
+// $parBirthday = CreateBirthday($_POST['year'], $_POST['month'], $_POST['day']);
+// $parTime = $_POST['tukin'];
+// $parHoliday = $_POST['holiday'];
+// $parRetire = $_POST['retire'];
+// $date = new DateTime('now');
+// $parRegister = $date->format('Y-m-d H:i:s');
+
+// $username = "postgres";
+// $password = "db-postgre-2951";
+// // (2) データベースに接続
+// $pdo = new PDO('pgsql:dbname=commuting;host=localhost', $username, $password);
+
+// // (3) SQL作成
+// $stmt = $pdo->prepare("INSERT INTO t_user (
+// 	birthday, time, holiday, retire, registerdate
+// ) VALUES (
+// 	:birthday, :time, :holiday, :retire, :registerdate
+// )");
+
+// // (4) 登録するデータをセット
+// $stmt->bindParam( ':birthday', $parBirthday, PDO::PARAM_STR);
+// $stmt->bindParam( ':time', $parTime, PDO::PARAM_STR);
+// $stmt->bindParam( ':holiday', $parHoliday, PDO::PARAM_STR);
+// $stmt->bindParam( ':retire', $parRetire, PDO::PARAM_STR);
+// $stmt->bindParam( ':registerdate', $parRegister, PDO::PARAM_STR);
+
+// // (5) SQL実行
+// $res = $stmt->execute();
+
+// // (6) データベースの接続解除
+// $pdo = null;
+
+// 生年月日から現在の年齢算出する
+// （（生）19980505-（現）20211107）/10000で切り捨てると、230602で23歳となる
+// $birthday = intval($parBirthday);
+$birthday = intval("19980225");
+$today = intval(date("Ymd", time()));
+$age = floor(($today - $birthday) / 10000);
+
+/*
   算出値
-  1週間の通勤時間
-  1ヶ月の通勤時間
-  年間通勤時間
-  10年間の通勤時間
-  定年までの通勤時間
+  1週間の通勤時間=1日*5
+  1ヶ月の通勤時間=((365日-年間休日)/12)*1日
+  年間通勤時間=(365日-年間休日)*1日
+  10年間の通勤時間=((365日-年間休日)*1日)*10年
+  定年までの通勤時間=(定年-現在=残り通勤年数)*365*1日
   */
 
-    $year = $_POST['year'];
-    $month = $_POST['month'];
-    $day = $_POST['day'];
+// 基準値
+$oneDayCommute = intval($_POST['tukin']) * 2;
+$yearWorkDay = 365 - intval($_POST['holiday']);
+// $workingYear = ((intval($_POST['retire'])-$age)*$yearWorkDay)*$oneDayCommute;
+define('JAPAN_AVERAGE','1時間19分');
+define('WORLD_AVERAGE','1時間5分');
 
-?>
+// 1日
+$strOneDay = MuniteToHourMinute($oneDayCommute);
+// 1週間
+$oneWeek = $oneDayCommute * 5;
+$strOneWeek = MuniteToHourMinute($oneWeek);
+// 1年
+$oneYear = $yearWorkDay * $oneDayCommute;
+$strOneYear = MuniteToHourMinute($oneYear);
+// 1ヶ月
+$oneMonth = round($oneYear / 12);
+$strOneMonth = MuniteToHourMinute($oneMonth);
+// 10年
+$tenYear = $oneYear * 10;
+$strTenYear = MuniteToHourMinute($tenYear);
+// 定年まで
+// $untilRetire = ($workingYear * $yearWorkDay) * $oneDayCommute;
+// $strUntilRetire = MuniteToHourMinute($untilRetire);
+
+
+/** 年月日を入れると、8桁の文字列作成 */
+function CreateBirthday($year, $month, $day)
+{
+  if (mb_strlen($month) == 1) {
+    $month = substr_replace($month, "0", 0, 0);
+  }
+  if (mb_strlen($day) == 1) {
+    $day = substr_replace($day, "0", 0, 0);
+  }
+  $result = $year . $month . $day;
+  return $result;
+}
+
+/** 分を入れると、分数に応じて〇日〇時間〇分か、〇時間〇分を返します */
+function MuniteToHourMinute($baseMinute)
+{
+  if ($baseMinute > 1440) {
+    $day = floor($baseMinute / 1440);
+    $baseMinute = $baseMinute - $day*1440;
+    
+    // if30で割れたらstr月を作る
+    $totalMonth = 0;
+    if ($day >= 30) {
+      $totalMonth = 0;
+    }
+
+    $strDay = $day . "日";
+    $strHour = floor($baseMinute / 60) . "時間";
+    $strMinute = ($baseMinute % 60) . "分";
+    $result = $strDay . $strHour . $strMinute;
+    return $result;
+  } else {
+    $hour = floor($baseMinute / 60) . "時間";
+    $minute = ($baseMinute % 60) . "分";
+    $result = $hour . $minute;
+    return $result;
+  }
+}
--- View/result.php
+++ View/result.php
@@ -13,8 +13,7 @@
   <script type="text/javascript" src="../js/result.js"></script>
   <!-- jQuery▲ -->
   <!-- CSS▼ -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   <link rel="stylesheet" href="../css/result.css">
   <!-- CSS▲ -->
 
@@ -22,47 +21,91 @@
 </head>
 
 <body>
-  <header>
-    <div>
-      <h3>通勤時間チェッカー</h3>
-      <label>あなたの通勤時間を算出します</label>
+  <header class="text-center pt-3 pb-3">
+    <div class="">
+      <!-- <a href="../View/index.html" class="text-decoration-none">
+        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-emoji-smile"
+          viewBox="0 0 16 16">
+          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
+          <path
+            d="M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z">
+          </path>
+        </svg>
+      </a> -->
+      <a href="../View/index.html" class="text-decoration-none">
+        <h3>通勤時間チェッカー</h3>
+      </a>
     </div>
+    <label>あなたが生涯通勤に使う時間を算出します。</label>
   </header>
 
-    <main>
+  <main>
+    <div class="col-10 container text-center">
+
+      <h5>現在<?php echo $age ?>歳のあなたが</h5>
+      <h5>通勤に消費する時間です。</h5>
+      <table class="table table-striped">
+        <!-- <thead>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col"></th>
+          </tr>
+        </thead> -->
+        <tbody>
+          <tr>
+            <th>1週間</th>
+            <td><?php echo $strOneWeek; ?></td>
+          </tr>
+          <tr>
+            <th>1ヶ月</th>
+            <td><?php echo $strOneMonth; ?></td>
+          </tr>
+          <tr>
+            <th>1年</th>
+            <td><?php echo $strOneYear; ?></td>
+          </tr>
+          <tr>
+            <th>10年</th>
+            <td><?php echo $strTenYear; ?></td>
+          </tr>
+          <!-- <tr>
+            <th>定年まで</th>
+            <td><?php echo $strUntilRetire; ?></td>
+          </tr> -->
+        </tbody>
+      </table>
+
       <div>
-        <h5>生涯通勤に使う時間</h5>        
-        <table>
-          <tbody>
-            <tr>
-              <th>1週間</th>
-              <td><?php echo $year; ?>時間</td>
-            </tr>
-            <tr>
-              <th>1ヶ月</th>
-              <td>〇時間</td>
-            </tr>
-            <tr>
-              <th>1年</th>
-              <td>〇時間</td>
-            </tr>
-            <tr>
-              <th>10年</th>
-              <td>〇時間</td>
-            </tr>
-            <tr>
-              <th>定年まで</th>
-              <td>〇時間</td>
-            </tr>
-          </tbody>
-        </table>
+        <h5>平均比較</h5>
       </div>
-    </div>
+
+      <table class="table table-striped">
+        <!-- <thead>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col"></th>
+          </tr>
+        </thead> -->
+        <tbody>
+          <tr>
+            <th>あなたの1日</th>
+            <td><?php echo $strOneDay; ?></td>
+          </tr>
+          <tr>
+            <th><a href="http://www.stat.go.jp/data/shakai/2016/rank/index.html" target="_blank" rel="noopener noreferrer">日本の平均</a></th>
+            <td><?php echo JAPAN_AVERAGE; ?></td>
+          </tr>
+          <tr>
+            <th><a href="https://news.mynavi.jp/article/20141029-a038/" target="_blank" rel="noopener noreferrer">世界の平均</a></th>
+            <td><?php echo WORLD_AVERAGE; ?></td>
+          </tr>
+        </tbody>
+      </table>
+    </d>
+
   </main>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
 
 </html>
